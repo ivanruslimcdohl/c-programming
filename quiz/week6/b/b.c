c4883704ba484730357f102865720fa76334d8b4
Ivan Rusli <ivanrusli777@gmail.com>
2020-10-10 16:31:12 +0700

    add b

--- quiz/week6/b/b.c
+++ quiz/week6/b/b.c
@@ -7,46 +7,37 @@
 // N = modulo
 
 // find size of cycle!
+// (a*b^i)%n
 
 int main()
 {
     int T;
-    long n, a, b;
+    int n, a, b;
     scanf("%d", &T);
     if (T < 1 || T > 100)
     {
         printf("error: these condition not satisfied: 1 <= T <= 100");
         return 0;
     }
+    printf("\n");
     for (int j = 0; j < T; j++)
     {
         scanf("%d %d %d", &n, &a, &b);
-        printf("n: %d, a: %d, b: %d", n, a, b);
+        printf("n: %d, a: %d, b: %d\n", n, a, b);
         if ((n < 1 || n > 100000) || (a < 1 || a > 100000) || (b < 1 || b > 100000))
         {
             printf("error: these condition not satisfied: 1 <= n,a,b <= 100,000");
             return 0;
         }
-        // int e1 = (((a % n) * (b % n)) % n);
-        // int e2 = ((a * b) % n);
-        // printf("e1: %d, e2: %d\n", e1, e2);
-        // if (e1 == e2)
-        // {
-        //     printf("equation valid!\n");
-        // }
-        // else
-        // {
-        //     printf("equation not valid!\n");
-        //     return 0;
-        // }
-        long i = 1;
+        int i = 1;
         for (;;)
         {
-            long next = (long)(a * pow(b, i)) % n;
-
+            int next = (int)(a * pow(b, i)) % n;
             printf("i = %d, a = %d, next: %d\n", i, a, next);
-            if (a == next)
+
+            if ((a == next) || (next <= 0))
             {
+                printf("cycle not found(?):\n");
                 printf("Case #%d: %d\n\n", j + 1, i);
                 break;
             }
